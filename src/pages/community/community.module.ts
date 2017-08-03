7b0dc077f6c637def402551e9af8c6ff644d89f1
hkb <hkb@tisynch.com>
2017-08-03 19:28:20 +0200

    minor change

--- src/pages/community/community.module.ts
+++ src/pages/community/community.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { CommunityPage } from './community';
-import {FabbuttonComponentModule} from "../../shared/components/fabbutton/fabbutton.module";
 import {HeaderComponentModule} from "../../shared/components/header/header.module";
 import {AudioItemComponentModule} from "../../shared/components/audio-item/audio-item.module";
 import {CommunityItemComponentModule} from "../../shared/components/community-item/community-item.module";
