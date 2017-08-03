f9a4d499b5e67df31b13843de70d9ec7419bfe8f
hkb <hkb@tisynch.com>
2017-08-03 15:58:38 +0200

    removed old tag form, replaced by alert

--- src/pages/tag-manager/tag-manager.module.ts
+++ src/pages/tag-manager/tag-manager.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TagManagerPage } from './tag-manager';
+import {GroupBy} from "../../pipes/group-by";
 
 @NgModule({
   declarations: [
     TagManagerPage,
+    GroupBy
   ],
   imports: [
     IonicPageModule.forChild(TagManagerPage),
--- src/pages/tag-manager/tag-manager.ts
+++ src/pages/tag-manager/tag-manager.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
 import {TagProvider} from "../../providers/tag";
+import {Observable} from "rxjs/Observable";
+import {Tag} from "../../models/tag";
 
 /**
  * Generated class for the TagManagerPage page.
@@ -17,14 +19,88 @@ export class TagManagerPage {
 
   tag = { name: '', description: '' };
 
+  public tags$: Observable<Tag[]>;
+  private _tags: Tag[];
+  private _subscrition: any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
-              private readonly tagProvider: TagProvider) {
+              private readonly tagProvider: TagProvider,
+              private readonly alertCtrl: AlertController) {
+
+    this.tags$ = tagProvider.tag$;
+    this.tagProvider.getTags();
+
+    this._subscrition = this.tags$.subscribe(tag => {
+      this._tags = tag;
+      console.log(this._tags);
+    });
+    console.log(this.tags$);
   }
 
-  save(){
-    console.log('Creating');
-    this.tagProvider.create(this.tag);
+  ionViewDidLoad() {
+    console.log('load');
+    console.log(this._tags);
   }
+
+  ionViewDidLeave(){
+    console.log('Left');
+    this._subscrition.unsubscribe();
+  }
+
+  promptForm() {
+    let alert = this.alertCtrl.create({
+      title: 'Nouveau Tag',
+      message: 'Entrez un nom et une description (optionnel) pour ce nouveau tag.',
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Nom'
+        },
+        {
+          name: 'description',
+          placeholder: 'Description'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Ajouter',
+          handler: data => {
+            if(data.name){
+              this.save(data);
+            }
+            else {
+              this.doAlert('Veuillez definir un nom pour ce tag.');
+              return false;
+            }
+            console.log(data);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  doAlert(message: string) {
+    let alert = this.alertCtrl.create({
+      title: 'Attention!',
+      subTitle: message,
+      buttons: ['OK']
+    });
+
+    alert.present();
+  }
+
+  save(tagData){
+    console.log('Saving tag.');
+    this.tagProvider.create(tagData);
+  }
+
 }
