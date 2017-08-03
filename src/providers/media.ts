a1e7cf7a227004525375dead922139be5125026a
hkb <hkb@tisynch.com>
2017-08-03 22:11:55 +0200

    implemented getPopularities

--- src/providers/media.ts
+++ src/providers/media.ts
@@ -1,31 +1,37 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers, RequestOptions} from "@angular/http";
 import { Storage } from '@ionic/storage';
-import { JwtHelper, AuthHttp } from 'angular2-jwt';
 import { Observable, BehaviorSubject } from "rxjs";
 import 'rxjs/add/operator/map';
 
 import { EndpointsProvider } from './endpoints';
 import { Media } from "../models/media";
+import { Popularity } from "../models/popularity";
 
 
 @Injectable()
 export class MediaProvider {
 
   public media$: Observable<Media[]>;
+  public popularities$: Observable<Popularity[]>;
   private _medias: BehaviorSubject<Media[]>;
+  private  _popularities: BehaviorSubject<Popularity[]>;
   private _dataStore: {  // This is where we will store our data in memory
-    medias: Media[]
+    medias: Media[],
+    popularities: Popularity[],
   };
 
   constructor(private readonly http: Http,
               private readonly storage: Storage,
               private readonly endpoints: EndpointsProvider) {
 
-    this._dataStore = { medias: [] };
-    this._medias = <BehaviorSubject<Media[]>>new BehaviorSubject([]);
-    this.media$ = this._medias.asObservable();
+    this._dataStore = { medias: [], popularities: [] };
 
+    this._medias = <BehaviorSubject<Media[]>>new BehaviorSubject([]);
+    this._popularities = <BehaviorSubject<Popularity[]>>new BehaviorSubject([]);
+
+    this.media$ = this._medias.asObservable();
+    this.popularities$ = this._popularities.asObservable();
   }
 
   getTokenInfo(): Promise<any>{
@@ -64,14 +70,45 @@ export class MediaProvider {
           data => {
             // add new datas to store.medias
             this._dataStore.medias = data.filter((media, index)=>{
-
-              if(index<= limit){
-                return media
-              }
+              return media
+              // if(index<= limit){
+              //   // this.http.get(this.endpoints.getPopularityForMedia(media._id), options)
+              //   //   .map(response => response.json())
+              //   //   .subscribe(
+              //   //     data => {
+              //   //       console.log(data);
+              //   //     }
+              //   //   );
+              //   console.log(media);
+              //   return media
+              // }
 
             });
             // assign new state to observable Medias Subject
             this._medias.next(Object.assign({}, this._dataStore).medias);
+          },
+          error => {console.log(error)}
+        );
+    });
+  }
+
+  getPopularities(){
+    this.storage.get('login_response').then(resp => {
+      let tokenObj = JSON.parse(resp);
+      console.log('Token : ' + tokenObj.token);
+      let headers: Headers = new Headers({'Authorization': 'JWT ' + tokenObj.token});
+      let options: RequestOptions = new RequestOptions({headers: headers});
+      this.http.get(this.endpoints.getPopularities(), options)
+        .map(response => response.json())
+        .subscribe(
+          data => {
+            // add new datas to store.medias
+            this._dataStore.popularities = data.filter((popularity, index)=>{
+              console.log(popularity);
+              return popularity;
+            });
+            // assign new state to observable Medias Subject
+            this._popularities.next(Object.assign({}, this._dataStore).popularities);
           },
           error => {console.log(error)}
         );
